edbb3949db9f75d5090f34265b1ccd675edc5db6
Zingy <https://github.com/YoussifAdelFathy>
2022-12-12 11:38:32 +0200

    viladiation and sing up

--- app/src/main/java/com/example/mobiledevproj/Singup2.java
+++ app/src/main/java/com/example/mobiledevproj/Singup2.java
@@ -1,22 +1,101 @@
 package com.example.mobiledevproj;
 
+import static java.lang.Integer.parseInt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import com.google.common.collect.Range;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 public class Singup2 extends AppCompatActivity {
+    private Boolean selected;
+    DatabaseReference ref = FirebaseDatabase.getInstance("https://hospital-app-be6c3-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup2);
+        RadioGroup Gender = (RadioGroup) findViewById(R.id.Gender);
+        RadioButton male,female;
+        male = (RadioButton) findViewById(R.id.Male);
+        female =(RadioButton) findViewById(R.id.Female);
+
+        Gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup Gender, int i) {
+
+                if (i == R.id.Male) {
+                    selected = true;
+                } else if (i == R.id.Female) {
+                    selected = false;
+                }
+
+
+            }});
+        EditText Chronic_Diseases = findViewById(R.id.Chronic_Diseases);
+        EditText Address = findViewById(R.id.Address);
+        EditText Age = findViewById(R.id.Age);
+        AwesomeValidation V1= new AwesomeValidation(ValidationStyle.BASIC);
+        V1.addValidation(this, R.id.Address, RegexTemplate.NOT_EMPTY, R.string.invalid_Address);
+        V1.addValidation(this, R.id.Age, RegexTemplate.NOT_EMPTY, R.string.invalid_Age);
+        V1.addValidation(this, R.id.Age, Range.closed(0, 130), R.string.invalid_age);
+        Intent from = getIntent();
+        String email = from.getStringExtra("email");
+        String username = from.getStringExtra("username");
+        String password = from.getStringExtra("password");
+
+
+
+
         Button resg;
         resg = findViewById(R.id.Register);
         resg.setOnClickListener(view -> {
-            Intent i = new Intent(this, profile.class);
-            startActivity(i);
+            if(V1.validate()){
+                String address = Address.getText().toString();
+                String disease = Chronic_Diseases.getText().toString();
+                ArrayList<String> chronic_diseases  = new ArrayList<>(Arrays.asList(disease.split(",")));
+                int death =chronic_diseases.size();
+                int age = Integer.parseInt(Age.getText().toString());
+                if (death >5){
+                    Toast.makeText(this, "you do not need hospital u need morgue", Toast.LENGTH_SHORT).show();
+                }
+                if(male.isChecked() || female.isChecked()){
+                    Patient p = new Patient(username, password, address, selected, chronic_diseases, email, age);
+                    int min = 1;
+                    int max =10000;
+                    int id = ThreadLocalRandom.current().nextInt(min, max + 1);
+                    String Id = Integer.toString(id);
+                    ref.child("users").child(Id).setValue(p);
+                    Intent i = new Intent(this, profile.class);
+
+                    startActivity(i);
+                }else {
+                    Toast.makeText(this, "either u have it or u do not Choose gender dear", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }else{
+                Toast.makeText(this, "Form Is not complete dear", Toast.LENGTH_LONG).show();
+            }
+
+
         });
     }
 }
--- app/src/main/java/com/example/mobiledevproj/signupPage.java
+++ app/src/main/java/com/example/mobiledevproj/signupPage.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 
 public class signupPage extends AppCompatActivity {
 
@@ -13,10 +20,39 @@ public class signupPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_page);
         Button btn;
+        EditText email;
+        email = findViewById(R.id.user_email_p);
+        EditText password;
+        password = findViewById(R.id.password_p);
+        EditText username;
+        username =findViewById(R.id.username_p);
+        EditText confirmpassword;
+        confirmpassword = findViewById(R.id.confirm_password_p);
+        String regexPassword = ".{7,}";
+        AwesomeValidation V= new AwesomeValidation(ValidationStyle.BASIC);
+        V.addValidation(this,R.id.user_email_p,Patterns.EMAIL_ADDRESS,R.string.invalid_email);
+        V.addValidation(this, R.id.username_p, RegexTemplate.NOT_EMPTY, R.string.invalid_name);
+        V.addValidation(this,R.id.user_email_p,RegexTemplate.NOT_EMPTY,R.string.Empty_Email);
+        V.addValidation(this,R.id.password_p,regexPassword,R.string.invalid_password);
+        V.addValidation(this,R.id.password_p,RegexTemplate.NOT_EMPTY,R.string.Empty_Password);
+        V.addValidation(this,R.id.confirm_password_p,RegexTemplate.NOT_EMPTY,R.string.Empty_Confirm_Password);
+        V.addValidation(this, R.id.confirm_password_p, R.id.password_p, R.string.invalid_confirm_password);
         btn = findViewById(R.id.log_in_p);
         btn.setOnClickListener(view -> {
-            Intent i = new Intent(this, Singup2.class);
-            startActivity(i);
+            if (V.validate()){
+                Intent i = new Intent(this, Singup2.class);
+                String getemail = email.getText().toString();
+                String getusername = username.getText().toString();
+                String getpassword = password.getText().toString();
+                i.putExtra("email",getemail);
+                i.putExtra("password",getpassword);
+                i.putExtra("username",getusername);
+                startActivity(i);
+
+            }else{
+                Toast.makeText(this, "Form Is not complete dear", Toast.LENGTH_LONG).show();
+            }
+
         });
     }
 }
