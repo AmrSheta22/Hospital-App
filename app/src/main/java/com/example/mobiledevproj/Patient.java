b9908e16ad4fbeca0160fc11903fa6327f3f50cf
tasneem <atasneem789@gmail.com>
2022-12-16 17:34:09 +0200

    data query for patient profile page

--- app/src/main/java/com/example/mobiledevproj/Patient.java
+++ app/src/main/java/com/example/mobiledevproj/Patient.java
@@ -1,35 +1,57 @@
 package com.example.mobiledevproj;
 
+import com.google.firebase.database.Exclude;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Patient {
-        public String id;
-        public String name;
-        public String password;
-        public String address;
-        public boolean gender;
-        public List diseases;
-        public String email;
-        public int age;
-        public Patient() {
-            // Default constructor required for calls to DataSnapshot.getValue(User.class)
-        }
+    public String id;
+    public String name;
+    public String password;
+    public String address;
+    public boolean gender;
+    public ArrayList<String> diseases;
+    public String email;
+    public int age;
+    public Map<String, Boolean> stars = new HashMap<>();
 
-        public Patient(String id, String name, String password, String address, boolean gender, List diseases, String email, int age) {
-            this.id = id;
-            this.name = name;
-            this.password = password;
-            this.address = address;
-            this.gender = gender;
-            this.diseases = diseases;
-            this.email = email;
-            this.age = age;
-        }
-
+    public Patient() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
+
+    public Patient(String id, String name, String password, String address, boolean gender, ArrayList<String> diseases, String email, int age) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.address = address;
+        this.gender = gender;
+        this.diseases = diseases;
+        this.email = email;
+        this.age = age;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("id", id);
+        result.put("name", name);
+        result.put("password", password);
+        result.put("gender", gender);
+        result.put("diseases", diseases);
+        result.put("email", email);
+
+        return result;
+    }
+
+    public String getinfo() {
+        return id+age+address+gender+email;
+    }
+}
 
     //String[] diseases = {"diabetes", "heart disease"};
     //List nameList = new ArrayList<String>(Arrays.asList(diseases));
