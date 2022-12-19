a91d99ae5d48e4002ab76b0492388b35f52d41de
Amr Sheta <amrsheta751@gmail.com>
2022-12-19 20:39:03 +0200

    mod to doctor class making

--- app/src/main/java/com/example/mobiledevproj/makeDoctor.java
+++ app/src/main/java/com/example/mobiledevproj/makeDoctor.java
@@ -55,6 +55,8 @@ public class makeDoctor extends AppCompatActivity {
                             if (task.isSuccessful()) {
 
                                 ref.child("doctors").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(new Doctor(FirebaseAuth.getInstance().getCurrentUser().getUid(), "doctor-bulk-billing-doctors-chapel-hill-health-care-medical-3.png", Name, Password, Department, Email, ageint, ratingint, numofpatients, yearsofexpint));
+                                Intent i = new Intent(makeDoctor.this, doctorOrPharmacist.class);
+                                startActivity(i);
                             }
                         }
                     });
