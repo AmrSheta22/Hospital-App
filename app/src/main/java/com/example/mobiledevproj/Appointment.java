d4d1a13f5f7c167bbe1545a05fae69d246fbe8f7
mhamoody <mhamoodykamal@gmail.com>
2022-12-21 03:11:38 +0200

    Akuma Kamal

--- app/src/main/java/com/example/mobiledevproj/Appointment.java
+++ app/src/main/java/com/example/mobiledevproj/Appointment.java
@@ -1,17 +1,30 @@
 package com.example.mobiledevproj;
 
 public class Appointment {
-    public String number;
     public String patientID;
     public String doctorID;
     // id is incremental so we don't need number of appointment
     public Appointment() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
-    public Appointment(String number, String patientID, String doctorID) {
-        this.number = number;
+    public Appointment(String patientID, String doctorID) {
         this.patientID = patientID;
         this.doctorID = doctorID;
+    }
+    public String getdoctorID() {
+        return doctorID;
+    }
+
+    public void setdoctorID(String doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    public String getpatientID() {
+        return patientID;
+    }
+
+    public void setpatientID(String patientID) {
+        this.patientID = patientID;
     }
 }
 
